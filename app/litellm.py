a44e8c6174edc3d53848a166538bac50dfcba150
sskskfskfj <dongkyun7717@gmail.com>
2025-11-11 17:11:27 +0900

    setting

--- app/litellm.py
+++ app/litellm.py
@@ -8,6 +8,7 @@ os.environ["HUGGINGFACE_API_KEY"] = settings.HUGGINGFACE_API_KEY
 os.environ["UI_USERNAME"] = settings.UI_USERNAME
 os.environ["UI_PASSWORD"] = settings.UI_PASSWORD
 os.environ["DATABASE_URL"] = settings.DATABASE_URL
+os.environ["LITELLM_MASTER_KEY"] = settings.LITELLM_MASTER_KEY
 
 def get_response(message):
     response = completion(
